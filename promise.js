a75213c63b42949253d488b7478c67834dc365bd
ranggaSuteja <shuteja@gmail.com>
2022-06-10 10:42:37 +0700

    feat: add counter audience  emotion

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  try {
+    const dataIXX = await promiseTheaterIXX();
+    const dataVGC = await promiseTheaterVGC();
+    const semuaData = [...dataIXX, ...dataVGC];
+    const result = semuaData.filter((e) => e.hasil === emotion);
+    return result.length;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
